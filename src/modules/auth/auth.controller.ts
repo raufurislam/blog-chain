ee7a237311828c5303485317af706a3e37512f52
raufurislam <raufurislam@gmail.com>
2025-09-15 13:59:48 +0600

    50-7 Email & Password Authentication

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { AuthService } from "./auth.service";
+
+const loginWithEmailAndPassword = async (req: Request, res: Response) => {
+  try {
+    const result = await AuthService.loginWithEmailAndPassword(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const AuthController = { loginWithEmailAndPassword };
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { prisma } from "../../config/db";
+
+const loginWithEmailAndPassword = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  console.log({ email, password });
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  if (password === user.password) {
+    return user;
+  } else {
+    throw new Error("Password is incorrect");
+  }
+};
+
+export const AuthService = { loginWithEmailAndPassword };
